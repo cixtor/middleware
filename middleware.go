3700326ecd88b896aa977dd514478bb755ee2193
Cixtor <cixtords@gmail.com>
2016-08-05 09:00:00 -0500

    Add function to return Middleware pointer and Node

--- middleware.go
+++ middleware.go
@@ -1,1 +1,29 @@
 package middlware
+
+import (
+	"net/http"
+	"time"
+)
+
+const DEFAULT_PORT = "8080"
+
+type Middleware struct {
+	Port         string
+	Nodes        map[string][]*Node
+	NotFound     http.Handler
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+type Node struct {
+	Path            string
+	Params          []string
+	NumParams       int
+	NumSections     int
+	Dispatcher      http.HandlerFunc
+	MatchEverything bool
+}
+
+func New() *Middleware {
+	return &Middleware{Nodes: make(map[string][]*Node)}
+}
