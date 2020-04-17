ecd71a4e187b93d356ff35e07f0d4f6e042f13d6
Cixtor <cixtords@gmail.com>
2020-04-17 09:00:00 -0800

    Add documentation for struct field Middleware.NotFound

--- middleware.go
+++ middleware.go
@@ -93,6 +93,20 @@ type Middleware struct {
 	// Ref: https://en.wikipedia.org/wiki/Server_log
 	Logger *log.Logger
 
+	// NotFound handles page requests to non-existing endpoints.
+	//
+	// The HTTP 404, 404 Not Found, 404, 404 Error, Page Not Found, File Not
+	// Found, or Server Not Found error message is a Hypertext Transfer
+	// Protocol (HTTP) standard response code, in computer network
+	// communications, to indicate that the browser was able to communicate
+	// with a given server, but the server could not find what was requested.
+	// The error may also be used when a server does not wish to disclose
+	// whether it has the requested information.
+	//
+	// The website hosting server will typically generate a "404 Not Found" web
+	// page when a user attempts to follow a broken or dead link; hence the 404
+	// error is one of the most recognizable errors encountered on the World
+	// Wide Web.
 	NotFound http.Handler
 
 	IdleTimeout time.Duration
