30bdadeef12b87d9c0bd696afb373470da97e041
Cixtor <cixtords@gmail.com>
2021-09-16 09:00:00 -0800

    Add endpoint type to represent an entire URL and its string form

---
+++ endpoint.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"net/http"
+	"path"
+	"strings"
+)
+
+// sep represents the endpoint folder separator.
+var sep string = "/"
+
+// endpoint is a data structure to keep the defined routes, named parameters
+// and HTTP handler. Some routes like the document root and static files might
+// set another property to force the ServeHTTP method to return immediately for
+// every match in the URL no matter if the named parameters do not match.
+type endpoint struct {
+	// Handler is an HTTP handler to process requests to this endpoint.
+	Handler http.Handler
+	// Folders is a list of segments in the URL with some properties.
+	Folders []folder
+	// Levels is the number of valid segments in the URL.
+	Levels int
+	// HasGlob is True if at least one of the folders has a global match mark.
+	HasGlob bool
+}
+
+// String returns the canonical version of the URL.
+func (e endpoint) String() string {
+	n := len(e.Folders)
+	arr := make([]string, n)
+	for i := 0; i < n; i++ {
+		arr[i] = string(e.Folders[i])
+	}
+	return path.Clean(strings.Join(arr, sep))
+}
