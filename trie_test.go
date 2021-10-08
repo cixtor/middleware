43c9b55baff7e300cbeb3a1adbf581f624f6e94a
Cixtor <cixtords@gmail.com>
2021-10-08 09:00:00 -0800

    Add test to verify integrity of the new trie structure

---
+++ trie_test.go
@@ -0,0 +1,53 @@
+package middleware
+
+import (
+	"testing"
+)
+
+func TestBasicTrie(t *testing.T) {
+	root := newTrie()
+
+	root.Insert("/")
+	root.Insert("/hello")
+	root.Insert("/hello/world")
+	root.Insert("/hello/world/how")
+	root.Insert("/hello/world/how/are")
+	root.Insert("/hello/world/how/are/you")
+
+	testCases := []struct {
+		found bool
+		query string
+	}{
+		{found: true, query: "/hello/world/how/are/you"}, /* <<< */
+		{found: false, query: "/hello/world/how/are/yo"},
+		{found: false, query: "/hello/world/how/are/y"},
+		{found: false, query: "/hello/world/how/are/"},
+		{found: true, query: "/hello/world/how/are"}, /* <<< */
+		{found: false, query: "/hello/world/how/ar"},
+		{found: false, query: "/hello/world/how/a"},
+		{found: false, query: "/hello/world/how/"},
+		{found: true, query: "/hello/world/how"}, /* <<< */
+		{found: false, query: "/hello/world/ho"},
+		{found: false, query: "/hello/world/h"},
+		{found: false, query: "/hello/world/"},
+		{found: true, query: "/hello/world"}, /* <<< */
+		{found: false, query: "/hello/worl"},
+		{found: false, query: "/hello/wor"},
+		{found: false, query: "/hello/wo"},
+		{found: false, query: "/hello/w"},
+		{found: false, query: "/hello/"},
+		{found: true, query: "/hello"}, /* <<< */
+		{found: false, query: "/hell"},
+		{found: false, query: "/hel"},
+		{found: false, query: "/he"},
+		{found: false, query: "/h"},
+		{found: true, query: "/"}, /* <<< */
+	}
+	for _, tc := range testCases {
+		t.Run(tc.query, func(t *testing.T) {
+			if root.Search(tc.query) != tc.found {
+				t.Fatalf("searching for %s should return %#v", tc.query, tc.found)
+			}
+		})
+	}
+}
