addd418fd8fbb63ddfa29f56b595ef94b99c21e5
Cixtor <cixtords@gmail.com>
2021-11-28 09:00:00 -0800

    Add test to verify integrity of trie parameter detection

--- trie_test.go
+++ trie_test.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -47,6 +48,76 @@ func TestTrieBasic(t *testing.T) {
 		t.Run(tc.query, func(t *testing.T) {
 			if wasFound, _ := root.Search(tc.query); wasFound != tc.found {
 				t.Fatalf("searching for %s should return %#v", tc.query, tc.found)
+			}
+		})
+	}
+}
+
+func TestTrieWithNamedParameters(t *testing.T) {
+	root := newPrivTrie()
+
+	root.Insert("/")
+	root.Insert("/home")
+	root.Insert("/about")
+	root.Insert("/contact-us")
+	root.Insert("/blog")
+	root.Insert("/blog/:postid")
+	root.Insert("/products")
+	root.Insert("/products/:itemid")
+	root.Insert("/login")
+	root.Insert("/register")
+	root.Insert("/user/:username/profile")
+	root.Insert("/user/settings")
+	root.Insert("/user/settings/:pageid/success")
+	root.Insert("/user/orders")
+	root.Insert("/user/orders/:orderid")
+
+	testCases := []struct {
+		found   bool
+		webpage string
+		params  map[string]string
+	}{
+		{found: true, webpage: "/", params: map[string]string{}},
+		{found: false, webpage: "/notfound"},
+		{found: true, webpage: "/home", params: map[string]string{}},
+		{found: true, webpage: "/about", params: map[string]string{}},
+		{found: true, webpage: "/contact-us", params: map[string]string{}},
+		{found: true, webpage: "/blog", params: map[string]string{}},
+		{found: false, webpage: "/blog/"},
+		{found: true, webpage: "/blog/post-1", params: map[string]string{"postid": "post-1"}},
+		{found: true, webpage: "/blog/post-2", params: map[string]string{"postid": "post-2"}},
+		{found: true, webpage: "/blog/post-3", params: map[string]string{"postid": "post-3"}},
+		{found: false, webpage: "/blog/post-4/hello-world"},
+		{found: true, webpage: "/products", params: map[string]string{}},
+		{found: false, webpage: "/products/"},
+		{found: true, webpage: "/products/item-1", params: map[string]string{"itemid": "item-1"}},
+		{found: true, webpage: "/products/item-2", params: map[string]string{"itemid": "item-2"}},
+		{found: true, webpage: "/products/item-3", params: map[string]string{"itemid": "item-3"}},
+		{found: false, webpage: "/products/item-4/foobar"},
+		{found: true, webpage: "/login", params: map[string]string{}},
+		{found: true, webpage: "/register", params: map[string]string{}},
+		{found: true, webpage: "/user/root/profile", params: map[string]string{"username": "root"}},
+		{found: false, webpage: "/user/root/profile/foobar"},
+		{found: true, webpage: "/user/-/profile", params: map[string]string{"username": "-"}},
+		{found: false, webpage: "/user/profile"},
+		{found: true, webpage: "/user/settings", params: map[string]string{}},
+		{found: false, webpage: "/user/settings/"},
+		{found: false, webpage: "/user/settings/foobar"},
+		{found: false, webpage: "/user/settings/foobar/"},
+		{found: true, webpage: "/user/settings/foobar/success", params: map[string]string{"pageid": "foobar"}},
+		{found: true, webpage: "/user/orders", params: map[string]string{}},
+		{found: false, webpage: "/user/orders/"},
+		{found: true, webpage: "/user/orders/order-1", params: map[string]string{"orderid": "order-1"}},
+		{found: false, webpage: "/user/orders/order-1/foobar"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.webpage, func(t *testing.T) {
+			wasFound, params := root.Search(tc.webpage)
+			if wasFound != tc.found {
+				t.Fatalf("searching for %s should return %#v", tc.webpage, tc.found)
+			}
+			if tc.found && !reflect.DeepEqual(params, tc.params) {
+				t.Errorf("Search(%q) returned %#v, expected %#v", tc.webpage, params, tc.params)
 			}
 		})
 	}
